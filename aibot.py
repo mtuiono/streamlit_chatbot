20f8b8cca93ecc397bb42a25f17ecd371d70d57a
Anthony M <m_tuiono@yahoo.com>
2025-08-01 14:29:11 -0700

    Update aibot.py

--- aibot.py
+++ aibot.py
@@ -84,8 +84,8 @@ if prompt := st.chat_input("What is up?"):
 
 
     # Display assistant response in chat message container
-with st.chat_message("assistant"):
-    response = st.write_stream(response_generator())
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
 
     # Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": response})
